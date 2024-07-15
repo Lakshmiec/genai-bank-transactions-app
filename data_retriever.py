13710d9c78b90fa3a81d49ea05a192941d083e9e
Sreelakshmi E C <lakshmiec98@gmail.com>
2024-07-15 20:14:14 +0800

    Update data_retriever.py

--- data_retriever.py
+++ data_retriever.py
@@ -26,24 +26,5 @@ def get_response_data(user_query):
     return intent, category, transaction_type, dates
 
 
-# # Sample user query
-# user_query = "What's the total amount I've spent today July 7 2023"
-# get_response_data(user_query)
-# # Call the appropriate function based on the intent
-# if intent == "RetrieveTransactionList":
-#     retrieve_transaction_list(dates)
-# elif intent == "RetrieveExpenditureSummary":
-#     retrieve_expenditure_summary(date_list)
-# elif intent == "RetrieveCategoryTransactions:
-#      retrieve_category_transactions(category,dates=None):
-# elif intent == "RetrieveCategoryTransactionTotal":
-#     retrieve_category_transaction_total(category, transaction_type)
-# elif intent == "RetrieveTransactions(dates)" :
-#     retrieve_transactions(dates)
-# elif intent == "RetrieveTransactionsAmount":
-#     retrieve_transactions(dates, transaction_type)
-# # Add more conditions for other intents
-# else:
-#     print(f"Unknown intent: {intent}")
 
 
